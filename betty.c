ea0c06548b943b0216199eb68234399e33ef9231
sulaimon-ade <okunoyesouleiman@gmail.com>
2023-07-24 15:45:01 -0700

    betty.c

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Hello - Prints Hellow world on the output
+ * return - Return always 0
+ */
+
+int main(void)
+{
+	printf("Hello, World");
+		return (0);
+}
